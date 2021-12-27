05a1757066c91584bfc6e779a5f0fe2ce6a2854f
Colin Francis <francico@oregonstate.edu>
2021-12-27 18:31:30 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Colin Francis
+ * @brief A binary search tree implementation in C
+ * @version 0.1
+ * @date 2021-12-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    return 0;
+}
